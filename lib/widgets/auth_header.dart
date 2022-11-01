17abe87df62598c5f5926a9b7e87252534a78cb8
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-01 01:37:06 +0100

    Restyle login screen

---
+++ lib/widgets/auth_header.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// Import own files
+import 'package:pdrnl_events_app/utils/color.dart';
+
+class AuthHeader extends StatelessWidget {
+  final String text;
+
+  const AuthHeader(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.3,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [orangeColors, orangeLightColors],
+            end: Alignment.bottomCenter,
+            begin: Alignment.topCenter,
+          ),
+          borderRadius:
+              const BorderRadius.only(bottomLeft: Radius.circular(100))),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Center(
+            child: Image.asset("assets/images/pdrnl_white.png", width: 250),
+          ),
+        ],
+      ),
+    );
+  }
+}
