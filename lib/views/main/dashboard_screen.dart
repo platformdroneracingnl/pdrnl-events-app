c5a2c607deb5105dc98a67787ce4ef415394f62e
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-07 05:08:08 +0100

    Add pull to refresh on dashboard (#10)
    * Pull to refresh the events data
    
    * Fix issue with user is null
    
    * Set const to full column children

--- lib/views/main/dashboard_screen.dart
+++ lib/views/main/dashboard_screen.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // Own package imports
-import 'package:pdrnl_events_app/widgets/home/events_upcoming.dart';
+import 'package:pdrnl_events_app/providers/auth_provider.dart';
+import 'package:pdrnl_events_app/providers/events_provider.dart';
 import 'package:pdrnl_events_app/widgets/home/events_header.dart';
+import 'package:pdrnl_events_app/widgets/home/events_upcoming.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
+
+  Future<void> _refreshHome(BuildContext context) async {
+    final auth = Provider.of<AuthProvider>(context, listen: false);
+    await Provider.of<EventsProvider>(context, listen: false)
+        .getEvents(auth.token);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.grey[100],
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          SizedBox(height: 75),
-          // Events header
-          HomeEventsHeader(),
-          SizedBox(height: 10),
-          UpcomingEvents(),
-        ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshHome(context),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                SizedBox(height: 10),
+                // Events header
+                HomeEventsHeader(),
+                SizedBox(height: 10),
+                UpcomingEvents(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
